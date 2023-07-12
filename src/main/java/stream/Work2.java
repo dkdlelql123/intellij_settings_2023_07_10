f92170a81fc5c9b62fbfdfb4d608155e5f3b6076
dkdlelql123 <dkdlelql123@naver.com>
2023-07-12 23:10:54 +0900

    work5 - Stream(filter)

--- src/main/java/stream/Work2.java
+++ src/main/java/stream/Work2.java
@@ -13,16 +13,31 @@ public class Work2 {
         IntStream.rangeClosed(0, 10).forEach(e-> System.out.println(e));
         IntStream.rangeClosed(100,107).forEach(System.out::println);
 
-        // 2곱하기
+        // 곱하기
         int[] arr = new int[]{1,2,3,4,5};
         for(int a : arr) System.out.println(a * 2);
 
         int[] newArr = Arrays.stream(arr).map(
                 e -> {return e*3;}
         ).toArray();
+         //int [] newArr = Arrays.stream(arr).map(e-> e*3).toArray();
 
         System.out.println("\nStream 곱하기 ");
         System.out.println(Arrays.toString(newArr));
+
+        // 홀수제외시키기
+        int[] newArr2 = Arrays.stream(arr).filter(e->e%2==0).toArray();
+
+        System.out.println("\nStream 홀수제외");
+        System.out.println(Arrays.toString(newArr2));
+
+
+
+        // 홀수제외시키기
+        int[] newArr3 = Arrays.stream(arr).filter(e->e%2==0).map(e->e+10).toArray();
+
+        System.out.println("\nStream 홀수제외 후 +10 ");
+        System.out.println(Arrays.toString(newArr3));
 
     }
 }
